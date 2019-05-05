a6750bf9c8c53e64f47171b0afff9ea917dccb38
Amir Al-Omari <amir.al.omari.work@gmail.com>
2019-05-05 22:32:40 +0300

    remove jwt.js

--- jwt.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const enc = jwt.sign({ userId: 123 }, "secret");
-console.log(enc, typeof enc);
-
-const dec = jwt.verify(enc, "secret");
-console.log(dec, typeof dec);
