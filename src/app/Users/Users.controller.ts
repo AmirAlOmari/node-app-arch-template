9acb00fd34ac15f830298ff88bd479e5f09d8688
Amir Al-Omari <amir.al.omari.work@gmail.com>
2019-05-05 23:13:24 +0300

    valid reponse structure added

--- src/app/Users/Users.controller.ts
+++ src/app/Users/Users.controller.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from "express";
 
 import { UsersService } from "./Users.service";
+import { C200, C500, ErrorCodes, HTTPResponse } from "./../../utils/http";
 
 export class UsersController {
 	constructor(private usersService: UsersService = new UsersService()) {}
 
 	public test123() {
 		return async (req: Request, res: Response) => {
+			let send: HTTPResponse;
 			this.usersService
 				.test123()
 				.then(result => {
-					res.status(200).send(result);
+					send = {
+						...C200,
+						data: result,
+					};
+					res.status(send.code).send(send);
 				})
 				.catch(error => {
-					res.status(500).send(error);
+					send = {
+						...C500,
+						errors: [ErrorCodes["0"]],
+					};
+					res.status(500).send(send);
 				});
 		};
 	}
